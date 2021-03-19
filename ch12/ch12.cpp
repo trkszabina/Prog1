fb4579423da4ea4e623179ec0cfb7c51b9e98355
trkszabina <72131659+trkszabina@users.noreply.github.com>
2021-03-19 13:15:43 +0200

    Update ch12.cpp

--- ch12/ch12.cpp
+++ ch12/ch12.cpp
@@ -26,8 +26,8 @@ Axis xa {Axis::x, Point{20,375}, 300, 10, "x tengely"};
  win.attach(sine);
  win.set_label("My window #4");
  win.wait_for_button();
-
  sine.set_color(Color::black);
+	
  Polygon poly;
  poly.add(Point{50,350});
  poly.add(Point{100,250});
@@ -43,49 +43,49 @@ Axis xa {Axis::x, Point{20,375}, 300, 10, "x tengely"};
  poly_rect.add(Point{150,200});
  poly_rect.add(Point{150,250});
  poly_rect.add(Point{50,250});
-
+ poly_rect.set_fill_color(Color::green);
  win.attach(poly_rect);
+ 
+	
  Rectangle r {Point{50,150}, 100, 50};
  win.attach(r);
  win.set_label("My window #6");
  win.wait_for_button();
-
  r.set_fill_color(Color::yellow);
- poly.set_style(Line_style(Line_style::dash,6));
- poly_rect.set_style(Line_style(Line_style::dash,4));
- poly_rect.set_fill_color(Color::green);
  win.set_label("My window #7");
  win.wait_for_button();
 
  Text t {Point{10,25}, "Hello, graphical world!"};
  t.set_color(Color::red);
+ t.set_font(Font::times_bold);
+ t.set_font_size(20);
  win.attach(t);
  win.set_label("My window #8");
  win.wait_for_button();
 
- t.set_font(Font::times_bold);
- t.set_font_size(20);
- win.set_label("My window #9");
- win.wait_for_button();
 
  Image ii {Point{100,50},"image.jpg"};
  win.attach(ii);
- win.set_label("My window #10");
+ win.set_label("My window #9");
  win.wait_for_button();
 
- ii.move(175,5);
- win.set_label("My window #11");
- win.wait_for_button();
 
  Circle c {Point{100,200},50};
- Mark m {Point{100,205},'x'};
- ostringstream oss;
- Image cal {Point{225,50},"name.gif"};
- cal.set_mask(Point{30,10},225,250);
-
+ c.set_fill_color(Color::green);
  win.attach(c);
- win.attach(m);
- win.attach(cal);
- win.set_label("My window #12");
+ win.set_label("My window #10");
  win.wait_for_button();
+ 
+ 
+ Ellipse e {Point{100,200},75, 25};
+ Mark m {Point{100,205},'x'};
+ e.set_fill_color(Color::green);
+ win.attach(e);
+ win.attach(m);
+ win.wait_for_button();
+	
+ ii.move(10,20);
+	
+ win.wait_for_button();
+ 
 }
