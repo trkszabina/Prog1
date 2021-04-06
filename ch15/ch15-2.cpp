081ee38cbf8f8459ef7c78197f5c82826f972cb9
trkszabina <72131659+trkszabina@users.noreply.github.com>
2021-04-06 12:48:57 +0300

    Update ch15-2.cpp

--- ch15/ch15-2.cpp
+++ ch15/ch15-2.cpp
@@ -4,96 +4,72 @@ using namespace std;
 
 struct Person
 {
-	private:
-	string first_name,last_name;
-	int age;
+	Person() {}
+	Person(string ff, string ll, int aa);
 
-	public:
-	Person(){};
-	Person(string f, string l, int a)
-	{
-		first_name=f;
-		last_name=l;
-		age=a;
-	}
+	string first_name() const { return f; }
+	string last_name() const { return l; }
+	int age() const { return a; }
 
-	//getters
-	string get_first_name() const {return first_name;}
-	string get_last_name() const {return last_name;}
-	int get_age() const {return age;}
-
-	//setters
-	void set_first_name(string n){first_name=n;}
-	void set_last_name(string n){last_name=n;}
-	void set_age(int a){age=a;}
+private:
+	string f;
+	string l;
+	int a;
 };
 
-	ostream& operator<< (ostream& os, Person& p)
-	{
-		os<<"Name: "<<p.get_first_name()<<" "<<p.get_last_name()<<" age: "<<p.get_age();
-		return os;
+Person::Person(string ff, string ll, int aa)
+	: f{ff}, l{ll}, a{aa}
+{
+	if (aa < 0 || aa > 150) error("Invalid age");
+	string name = ff + ll;
+	for ( char c : name ){
+		switch(c){
+			case ';': case ':': case '"': case '\'': case '[': case ']': case '*': 
+			case '&': case '^': case '%': case '$': case '#': case '@': case '!':
+			error("Invalid character(s)");
+		}
+
 	}
+}
+ostream& operator<<(ostream& os, Person& p)
+{
+	return os << p.first_name() << ' ' << p.last_name() << ' ' << p.age();
+}
 
-	istream& operator>> (istream& is, Person& p)
-	{
-		string first,last;
-		int a;
-		is>>first>>last;
+istream& operator>>(istream& is, Person& p)
+{
+	string f;
+	string l;
+	int a;
+	is >> f >> l >> a;
 
-		for(auto letter : first)
-		{
-			if(!isalpha(letter) && !isdigit(letter))
-			{
-				cout<<letter<< " letter"<<endl;
-				error("Nem megfelelő név");
-			}
-		}
-		p.set_first_name(first);
+	if(!is) error("input error...");
 
-		for(auto letter : last)
-		{
-			if(!isalpha(letter))
-			{
-				cout<<letter<< " letter"<<endl;
-				error("Nem megfelelő név, adjon meg vezetéknevet is!");
-			}
-		}
-		p.set_last_name(last);
+	p = Person(f, l, a);
 
-		is>>a;
+	return is;
+}
 
-		if(0<=a && a<=150)
-			{
-				p.set_age(a);
-			}
-		else
-		{
-			error("Nem megfelelő kor");
-		}
-		return is;
-	}
+
+
 
 int main()
 {
 
 
-	Person p2;
+    vector<Person> persons;
+    Person p4;
 
-	cout<<"Give name and age:"<<endl;
-	cin>>p2;
-	cout<<p2<<endl;
+    cout << "Please enter the name and age(First name, Second name, Age): " << endl;
 
-	cout<<"Give names to the vector:"<<endl;
-
-	vector<Person> people;
-	Person temp;
-	int numOfPeople=0;
-	while(cin>>temp)
-	{
-		people.push_back(temp);
-		cout<<people[numOfPeople]<<endl;
-	}
-
-
-	return 0;
+    while (cin>>p4)
+    {
+       persons.push_back(p4);
+    }
+    for (int i = 0; i<persons.size(); ++i)
+    {
+      cout << persons[i] << endl;
+    }       
+    
+    keep_window_open();
 }
