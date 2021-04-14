1b0dcb8b2c5397983611f889289aff7a2ccb6d55
trkszabina <72131659+trkszabina@users.noreply.github.com>
2021-04-14 13:57:59 +0300

    Update Lines_window.cpp

--- ch16/Lines_window.cpp
+++ ch16/Lines_window.cpp
@@ -31,16 +31,16 @@ private:
 	void blue_pressed() { change_c(Color::blue); hide_color(); }
 	void black_pressed() { change_c(Color::black); hide_color(); }
 	
-    void dot_pressed() { change_s(Line_style::dot), hide_style(); }
+    	void dot_pressed() { change_s(Line_style::dot), hide_style(); }
 	void dash_pressed() { change_s(Line_style::dash), hide_style(); }
 	void solid_pressed() { change_s(Line_style::solid), hide_style(); }
     
-    void change_c(Color c) { lines.set_color(c); }
+   	void change_c(Color c) { lines.set_color(c); }
 	void change_s(Line_style s) { lines.set_style(s); }
     
-    void color_pressed() { color_button.hide(); color_menu.show(); }
+    	void color_pressed() { color_button.hide(); color_menu.show(); }
 	void style_pressed(){ style_button.hide(); style_menu.show(); }
-    void hide_color() { color_menu.hide(); color_button.show(); }	
+    	void hide_color() { color_menu.hide(); color_button.show(); }	
 	void hide_style() { style_menu.hide(); style_button.show(); }
     
      };
