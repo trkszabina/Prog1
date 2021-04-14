bafc69585c76feab35533e852732eec713fb1c39
trkszabina <72131659+trkszabina@users.noreply.github.com>
2021-04-14 12:38:48 +0300

    Update Lines_window.cpp

--- ch16/Lines_window.cpp
+++ ch16/Lines_window.cpp
@@ -1,24 +1,68 @@
-#include "Lines_window.h"
 
-Lines_window::Lines_window(Point xy, int w, int h, const string& title)
-    :Window{xy,w,h,title},
+#include "GUI.h"
+#include "std_lib_facilities.h"
 
-next_button{Point{x_max()-300,0}, 150, 40, "Next point",
-        [](Address, Address pw) {reference_to<Lines_window>(pw).next();}},
-quit_button{Point{x_max()-150,0}, 150, 40, "Quit",
-        [](Address, Address pw) {reference_to<Lines_window>(pw).quit();}},
+using namespace Graph_lib;
 
- next_x{Point{300,0}, 50, 40, "next x:"},
- next_y{Point{450,0}, 50, 40, "next y:"},
- xy_out{Point{100,0}, 100, 40, "current (x,y):"},
+struct Lines_window : Graph_lib::Window
+{
+	Lines_window(Point xy, int w, int h, const string& title);
+private:
 
-color_menu{Point{x_max()-70,40},70,20,Menu::vertical,"color"},
-line_menu{Point{x_max()-70,60},70,20,Menu::vertical,"linestyle"},
-color_button{Point{x_max() - 80, 30}, 80, 20, "color menu",
-        [](Address, Address pw) {reference_to<Lines_window>(pw).color_pressed();},
-style_button{Point{0, 30}, 80, 20, "style menu",
-        [](Address, Address pw) {reference_to<Lines_window>(pw).style_pressed();}
-             
+	Open_polyline lines;
+
+	// widgets
+	Button next_button;
+	Button quit_button;
+	In_box next_x;
+	In_box next_y;
+	Out_box xy_out;
+	Menu color_menu;
+	Button color_button;
+	Menu style_menu;
+	Button style_button;
+
+	void next();
+	void quit();
+
+
+
+	void red_pressed() { change_c(Color::red); hide_color(); }
+	void blue_pressed() { change_c(Color::blue); hide_color(); }
+	void black_pressed() { change_c(Color::black); hide_color(); }
+	
+    void dot_pressed() { change_s(Line_style::dot), hide_style(); }
+	void dash_pressed() { change_s(Line_style::dash), hide_style(); }
+	void solid_pressed() { change_s(Line_style::solid), hide_style(); }
+    
+    void change_c(Color c) { lines.set_color(c); }
+	void change_s(Line_style s) { lines.set_style(s); }
+    
+    void color_pressed() { color_button.hide(); color_menu.show(); }
+	void style_pressed(){ style_button.hide(); style_menu.show(); }
+    void hide_color() { color_menu.hide(); color_button.show(); }	
+	void hide_style() { style_menu.hide(); style_button.show(); }
+    
+     };
+
+
+
+    Lines_window::Lines_window(Point xy, int w, int h, const string& title)
+	: Window{xy, w, h, title},
+	next_button{Point{x_max()-150,0}, 70, 20, "Next point",
+	[](Address, Address pw) {reference_to<Lines_window>(pw).next();}},
+	quit_button{Point{x_max()-70, 0}, 70, 20, "Quit",
+	[](Address, Address pw) {reference_to<Lines_window>(pw).quit();}},
+	next_x{Point{x_max()-310, 0}, 50, 20, "next x:"},
+	next_y{Point{x_max()-210, 0}, 50, 20, "next y:"},
+	xy_out{Point{100, 0}, 100, 20, "current (x,y):"},
+    color_menu{Point{x_max() - 70, 30}, 70, 20, Menu::vertical, "color"},
+    color_button{Point{x_max() - 80, 30}, 80, 20, "color menu",
+    [](Address, Address pw) {reference_to<Lines_window>(pw).color_pressed();}},
+    style_menu{Point{10, 30}, 70, 20, Menu::vertical, "style"},
+    style_button{Point{0, 30}, 80, 20, "style menu",
+    [](Address, Address pw) {reference_to<Lines_window>(pw).style_pressed();}}
+
 
 {
     attach(next_button);
@@ -26,13 +70,12 @@ style_button{Point{0, 30}, 80, 20, "style menu",
     attach(next_x);
     attach(next_y);
     attach(xy_out);
-    attach(lines);
-
+    
     color_menu.attach(new Button{Point{0, 0}, 0, 0, "red",
         [](Address, Address pw) {
             reference_to<Lines_window>(pw).red_pressed();
         }
-    });
+     });
     color_menu.attach(new Button{Point{0, 0}, 0, 0, "blue",
         [](Address, Address pw) {
             reference_to<Lines_window>(pw).blue_pressed();
@@ -58,29 +101,38 @@ style_button{Point{0, 30}, 80, 20, "style menu",
             reference_to<Lines_window>(pw).solid_pressed();
         }
     });
-
+    
     attach(color_menu);
-    attach(line_menu);
+    attach(style_menu);
     color_menu.hide();
-    line_menu.hide();
+    style_menu.hide();
     attach(color_button);
-    attach(linestyle_button);
-
-
+    attach(style_button);
+    attach(lines);
 }
 
 void Lines_window::quit()
 {
-    hide();
+	hide();
 }
 
 void Lines_window::next()
 {
-    int x = next_x.get_int();
-    int y = next_y.get_int();
+	int x = next_x.get_int();
+	int y = next_y.get_int();
 
-    lines.add(Point{x,y});
-  
-    ostringstream ss;
-    ss << '(' << x << ',' << y << ')';
-    xy_out.put(ss.str());
+	lines.add(Point{x, y});
+	ostringstream ss;
+	ss << '(' << x << ',' << y << ')';
+	xy_out.put(ss.str());
+
+	redraw();
+}
+
+int main()
+{
+	Lines_window win {Point{100, 100}, 600, 400, "lines"};
+	return gui_main();
+}
+    
+
